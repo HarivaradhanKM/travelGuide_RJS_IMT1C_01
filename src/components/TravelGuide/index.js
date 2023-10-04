cdfbb1214372c2452dc0b94584907d5dbfe1c3cd
HarivaradhanKM <harivaradhankm@gmail.com>
2023-10-04 08:25:49 +0000

    finalupdate_IMT1C_01

---
+++ src/components/TravelGuide/index.js
@@ -0,0 +1,85 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TravelPlaceList from '../TravelPlaceList'
+
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class TravelGuide extends Component {
+  state = {placeList: [], apiStatus: apiStatusConstants}
+
+  componentDidMount() {
+    this.getTravelPlaces()
+  }
+
+  renderFormattedData = data => ({
+    id: data.id,
+    name: data.name,
+    imageUrl: data.image_url,
+    description: data.description,
+  })
+
+  getTravelPlaces = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const apiUrl = 'https://apis.ccbp.in/tg/packages'
+    const response = await fetch(apiUrl)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedList = fetchedData.packages.map(eachPackage =>
+        this.renderFormattedData(eachPackage),
+      )
+      console.log(updatedList)
+      this.setState({
+        placeList: updatedList,
+        apiStatus: apiStatusConstants.success,
+      })
+    }
+  }
+
+  renderLoader = () => (
+    <div data-testid="loader" className="position">
+      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+    </div>
+  )
+
+  renderPlaceView = () => {
+    const {placeList} = this.state
+    return (
+      <ul className="places-list-container">
+        {placeList.map(eachPlace => (
+          <TravelPlaceList eachPlace={eachPlace} key={eachPlace.id} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderViewBasedOnApiStatus = () => {
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderPlaceView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoader()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="travel-guide-container">
+        <h1 className="main-heading">Travel Guide</h1>
+        <hr className="separator" />
+        {this.renderViewBasedOnApiStatus()}
+      </div>
+    )
+  }
+}
+
+export default TravelGuide
